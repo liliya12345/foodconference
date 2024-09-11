b4b972c7363ebc46204b4d81621b54a26c548786
liliya12345 <vapioo12345>
2024-09-11 11:15:32 +0200

    Добали разметку для для Navbar

--- front/conferensfood/src/components/Hero.js
+++ front/conferensfood/src/components/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import deliveryman from '../img/custom_img_960x1250_crop_center.webp'
 
 export default function Hero() {
   return (
@@ -14,26 +15,28 @@ export default function Hero() {
                         </div>
                     </div>
                     <div className="col-md-12 col-lg-5">
-                        <div id="carouselId" className="carousel slide position-relative" data-bs-ride="carousel">
-                            <div className="carousel-inner" role="listbox">
-                                <div className="carousel-item active rounded">
-                                    <img src="img/hero-img-1.png" className="img-fluid w-100 h-100 bg-secondary rounded" alt="First slide"/>
-                                    <a href="#" className="btn px-4 py-2 text-white rounded">Fruites</a>
-                                </div>
-                                <div className="carousel-item rounded">
-                                    <img src="img/hero-img-2.jpg" className="img-fluid w-100 h-100 rounded" alt="Second slide"/>
-                                    <a href="#" className="btn px-4 py-2 text-white rounded">Vesitables</a>
-                                </div>
-                            </div>
-                            <button className="carousel-control-prev" type="button" data-bs-target="#carouselId" data-bs-slide="prev">
-                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span className="visually-hidden">Previous</span>
-                            </button>
-                            <button className="carousel-control-next" type="button" data-bs-target="#carouselId" data-bs-slide="next">
-                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span className="visually-hidden">Next</span>
-                            </button>
-                        </div>
+                        {/*<div id="carouselId" className="carousel slide position-relative" data-bs-ride="carousel">*/}
+                        {/*    <div className="carousel-inner" role="listbox">*/}
+                        {/*        <div className="carousel-item active rounded">*/}
+                        {/*            <img src="img/hero-img-1.png" className="img-fluid w-100 h-100 bg-secondary rounded" alt="First slide"/>*/}
+                        {/*            <a href="#" className="btn px-4 py-2 text-white rounded">Fruites</a>*/}
+                        {/*        </div>*/}
+                        {/*        <div className="carousel-item rounded">*/}
+                        {/*            <img src="img/hero-img-2.jpg" className="img-fluid w-100 h-100 rounded" alt="Second slide"/>*/}
+                        {/*            <a href="#" className="btn px-4 py-2 text-white rounded">Vesitables</a>*/}
+                        {/*        </div>*/}
+                        {/*    </div>*/}
+                        {/*    <button className="carousel-control-prev" type="button" data-bs-target="#carouselId" data-bs-slide="prev">*/}
+                        {/*        <span className="carousel-control-prev-icon" aria-hidden="true"></span>*/}
+                        {/*        <span className="visually-hidden">Previous</span>*/}
+                        {/*    </button>*/}
+                        {/*    <button className="carousel-control-next" type="button" data-bs-target="#carouselId" data-bs-slide="next">*/}
+                        {/*        <span className="carousel-control-next-icon" aria-hidden="true"></span>*/}
+                        {/*        <span className="visually-hidden">Next</span>*/}
+                        {/*    </button>*/}
+                        {/*</div>*/}
+                        <img src ={deliveryman} width="549px" height="750px"/>
+
                     </div>
                 </div>
             </div>
--- front/conferensfood/src/components/MyNavbar.js
+++ front/conferensfood/src/components/MyNavbar.js
@@ -1,9 +1,9 @@
 import React, {useState} from "react";
-import {Link} from "react-router-dom";
-import {Button, Container, Nav, Navbar} from "react-bootstrap";
+import {Button, Container, Navbar} from "react-bootstrap";
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import axios from 'axios';
+import LanguagesAndLoggin from "./LanguagesAndLoggin";
 
 export default function MyNavbar() {
     const [show, setShow] = useState(false);
@@ -79,44 +79,48 @@ export default function MyNavbar() {
                             <span className="fa fa-bars text-primary"></span>
                         </button>
                         <Navbar.Collapse className="bg-white" id="navbarCollapse">
-                            <Nav className="mx-auto">
-                                <Link to="/" className="nav-item nav-link active">Home</Link>
-                                <a href="#shop" className="nav-item nav-link">Shop</a>
-                                <a href="shop-detail.html" className="nav-item nav-link">Shop Detail</a>
-                                <div className="nav-item dropdown">
-                                    <a href="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
-                                    <div className="dropdown-menu m-0 bg-secondary rounded-0">
-                                        <a href="cart.html" className="dropdown-item">Cart</a>
-                                        <a href="chackout.html" className="dropdown-item">Chackout</a>
-                                        <a href="testimonial.html" className="dropdown-item">Testimonial</a>
-                                        <a href="404.html" className="dropdown-item">404 Page</a>
-                                    </div>
-                                </div>
-                                <a href="contact.html" className="nav-item nav-link">Contact</a>
+                            {/*<Nav className="mx-auto">*/}
+                            {/*    <Link to="/" className="nav-item nav-link active">Home</Link>*/}
+                            {/*    <a href="#shop" className="nav-item nav-link">Shop</a>*/}
+                            {/*    <a href="shop-detail.html" className="nav-item nav-link">Shop Detail</a>*/}
+                            {/*    <div className="nav-item dropdown">*/}
+                            {/*        <a href="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>*/}
+                            {/*        <div className="dropdown-menu m-0 bg-secondary rounded-0">*/}
+                            {/*            <a href="cart.html" className="dropdown-item">Cart</a>*/}
+                            {/*            <a href="chackout.html" className="dropdown-item">Chackout</a>*/}
+                            {/*            <a href="testimonial.html" className="dropdown-item">Testimonial</a>*/}
+                            {/*            <a href="404.html" className="dropdown-item">404 Page</a>*/}
+                            {/*        </div>*/}
+                            {/*    </div>*/}
+                            {/*    <a href="contact.html" className="nav-item nav-link">Contact</a>*/}
 
-                            </Nav>
+                            {/*</Nav>*/}
                             <div className="d-flex m-3 me-0">
-                                <button
-                                    className="btn-search btn border border-secondary btn-md-square rounded-circle bg-white me-4"
-                                    data-bs-toggle="modal"
-                                    data-bs-target="#searchModal"
-                                >
-                                    <i className="fas fa-search text-primary"></i>
-                                </button>
-                                <a href="#" className="position-relative me-4 my-auto">
-                                    <i className="fa fa-shopping-bag fa-2x"></i>
+                                {/*<button*/}
+                                {/*    className="btn-search btn border border-secondary btn-md-square rounded-circle bg-white me-4"*/}
+                                {/*    data-bs-toggle="modal"*/}
+                                {/*    data-bs-target="#searchModal"*/}
+                                {/*>*/}
+                                {/*    <i className="fas fa-search text-primary"></i>*/}
+                                {/*</button>*/}
+                                <LanguagesAndLoggin/>
+                                {/*<a href="#" className="position-relative me-4 my-auto">*/}
+                                {/*    <i className="fa fa-shopping-bag fa-2x"></i>*/}
                                     {/* <span
                 className="position-absolute bg-secondary rounded-circle d-flex align-items-center justify-content-center text-dark px-1"
                  style="top: -5px; left: 15px; height: 20px; min-width: 20px;"
                 >
                   3
                 </span> */}
-                                </a>
+                                {/*</a>*/}
                                 {/*<a href="#" className="my-auto">*/}
 
-                                <button className=" my-auto fas fa-user fa-2x" onClick={handleShow}></button>
+                                {/*<button className=" my-auto fas fa-user fa-2x" onClick={handleShow}></button>*/}
 
                                 {/*</a>*/}
+
+
+
                             </div>
                         </Navbar.Collapse>
                     </Navbar>
@@ -130,15 +134,15 @@ export default function MyNavbar() {
                     <Form>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                             <Form.Label>Änvändarnamn</Form.Label>
-                            <Form.Control onChange={(e)=>setLogin(e.target.value)}
-                                type="text"
-                                autoFocus
+                            <Form.Control onChange={(e) => setLogin(e.target.value)}
+                                          type="text"
+                                          autoFocus
                             />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
                             <Form.Label>Lösenord</Form.Label>
-                            <Form.Control onChange={(e)=>setPassword(e.target.value)}
-                                type="password"
+                            <Form.Control onChange={(e) => setPassword(e.target.value)}
+                                          type="password"
                             />
                         </Form.Group>
                     </Form>
